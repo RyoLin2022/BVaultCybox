5d5e872d4e42c357bd85a747801226ac504938a2
Ryo <samxyz258456@gmail.com>
2022-10-09 05:03:23 +0800

    connectWallet

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,13 +3,67 @@ import React , { useState , useEffect } from 'react';
 import { Link , NavLink } from 'react-router-dom';
 import menus from '../../pages/menu';
 
+import { ethers } from 'ethers'
 import './styles.scss';
 import logo from '../../assets/images/logo/logo.png'
 import Button from '../button';
 
-
+let currentAccount = null;
 
 const Header = () => {
+    connectWallet();
+
+    const [WalletAddress, setWalletAddress] = useState(null)
+    async function changingAccount() {
+        if (window.ethereum) {
+            window.ethereum.on('accountsChanged', () => {
+                window.sessionStorage.removeItem('Account');
+                requestAccount()
+                window.location.reload();
+                requestAccount()
+            })
+        }
+    }
+
+    useEffect(() => {
+        changingAccount()
+    }, [WalletAddress])
+
+    async function requestAccount() {
+        console.log('Requesting account...')
+        try {
+            const accounts = await window.ethereum.request({
+                method: 'eth_requestAccounts',
+            })
+            currentAccount = accounts[0]
+            setWalletAddress(accounts[0])
+            sessionStorage.setItem('Account', currentAccount)
+            console.log("current account " + currentAccount)
+            var btnConnect = document.getElementById('connect-btn')
+            let lengthAcc = currentAccount.length
+            btnConnect.value = currentAccount
+            btnConnect.innerText =
+                currentAccount.substring(0, 4) + '...' + currentAccount.substring(lengthAcc - 4, lengthAcc)
+        } catch (error) {
+            console.log('error connecting')
+        }
+        //Check if Metamask Exist
+        if (window.ethereum) {
+            console.log('detected')
+        } else {
+            console.log('not detected')
+        }
+    }
+
+    async function connectWallet() {
+        if (typeof window.ethereum !== 'undefined') {
+            await requestAccount()
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            console.log('Wallet connected successfully!')
+        } else {
+            console.log('Please install an injected Web3 wallet')
+        }
+    }
 
     const [scroll, setScroll] = useState(false);
         useEffect(() => {
@@ -65,7 +119,11 @@ const Header = () => {
                             }
                         </ul>
                     </nav>
-                    <Button title='join discord' path='/contact' />
+                    
+                    <button className="tf-button btn-effect" onClick={connectWallet}>
+                        <span className="boder-fade"></span>
+                        <span className="effect" id="connect-btn">Connect Wallet</span>
+                    </button>
 
                     <div className={`mobile-button ${menuActive ? 'active' : ''}`} onClick={handleMenuActive}><span></span></div>
                 </div>
